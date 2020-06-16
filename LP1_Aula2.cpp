ac74eae5a68cb90ee988a2582f9d7c039542c4a8
jpqm <36828739+jpqm@users.noreply.github.com>
2020-06-16 10:13:53 -0300

    Update LP1_Aula2.cpp

--- LP1_Aula2.cpp
+++ LP1_Aula2.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main(int argc, char *argv[]){
 
     string mens;
-    string add = "add";
 
     if(argc == 1 || add.compare(argv[1])) {
 
@@ -15,7 +14,7 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    if(argc == 2 && argv[1] == add){
+    if(argc == 2 && argv[1] == "add"){
 
         cout << "Digite sua mensagem" << endl;
 
@@ -26,7 +25,11 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
-    cout << "Sua mensagem: " << argv[2] << endl;
+    if(argc == 3 && argv[1] == "add"){
+
+        cout << "Sua mensagem: " << argv[2] << endl;
+
+    }
 
     return 0;
 }
